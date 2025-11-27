e789f3b26c75f1f0af7f74fe55d6536d7a261449
Caio César <caio.cesar.g.souza@gmail.com>
2025-11-27 13:11:31 +0100

    simple spec entity

--- schema-validation/src/core.clj
+++ schema-validation/src/core.clj
@@ -1,4 +1,29 @@
-(ns core)
+(ns core
+  (:require [clojure.spec.alpha :as s]))
+
+;; Spec Schema
+;; ProcessExecution {id, process-id, started-at, finished-at, state = {running, complete, failed}}
+;; Process {id, name, created-at}
+
+(s/def :process/id uuid?)
+(s/def :process/name string?) ;; not empty?
+(s/def :process/created_at inst?) ;; not empty?
+(s/def :entity/process
+  (s/keys
+   :req-un [:process/id :process/name :process/created_at]))
 
 (defn -main []
-  (println "Hello!"))
+  (println "Hello"))
+
+(comment
+  (s/conform :process/id (random-uuid)) ;; true
+  (s/conform :process/id (str (random-uuid))) ;; invalid, we need coercion
+
+  (def process-1 {:id (random-uuid)
+                  :name "hello"
+                  :created_at (java.util.Date.)})
+
+  (s/conform :entity/process {:id (random-uuid)
+                              :name "foobar"
+                              :created_at (java.util.Date.)})
+  :hodor)
