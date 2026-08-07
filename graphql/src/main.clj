a087792ab17699c4f52f6f52cb3502d2f1fa3ac1
Caio César <caio.cesar.g.souza@gmail.com>
2026-08-07 09:15:15 +0200

    graphql #2 interop

--- graphql/src/main.clj
+++ graphql/src/main.clj
@@ -1,28 +1,38 @@
 (ns main
-      (:require [clojure.pprint :refer [pprint]])
-    (:import
-      graphql.ExecutionResult
-      graphql.GraphQL
-      graphql.schema.GraphQLSchema
-      graphql.schema.StaticDataFetcher
-      graphql.schema.idl.RuntimeWiring
-      graphql.schema.idl.SchemaGenerator
-      graphql.schema.idl.SchemaParser
-      graphql.schema.idl.TypeDefinitionRegistry))
+  (:require [clojure.pprint :refer [pprint]])
+  (:import graphql.ExecutionResult
+           graphql.GraphQL
+           graphql.schema.GraphQLSchema
+           graphql.schema.StaticDataFetcher
+           graphql.schema.DataFetcher
+           graphql.schema.idl.TypeRuntimeWiring$Builder
+           graphql.schema.idl.RuntimeWiring
+           graphql.schema.idl.SchemaGenerator
+           graphql.schema.idl.SchemaParser
+           graphql.schema.idl.TypeDefinitionRegistry))
 
-(def schema "type Query{hello: String}")
+(def schema (slurp "resources/schema.graphql"))
 (def parser (SchemaParser.))
 (def definition (.parse parser schema))
 
+(defn resolve-hello [b]
+  (.dataFetcher b "hello" (StaticDataFetcher. "world")))
+
+(defn book-definition [^TypeRuntimeWiring$Builder builder]
+  (.dataFetcher builder "books" (StaticDataFetcher. [{"id" 1 "title" "Book 1"}])))
+
+; Wire
 (def runtime (-> (RuntimeWiring/newRuntimeWiring)
-                 (.type "Query" (fn [b] (.dataFetcher b "hello" (StaticDataFetcher. "world"))))
+                 (.type "Query" book-definition)
                  (.build)))
 
 (def generator (SchemaGenerator.))
 (def graphql-schema (.makeExecutableSchema generator definition runtime))
-
 (def build (.build (GraphQL/newGraphQL graphql-schema)))
 
-(def result (.execute build "{hello}"))
+;; Query
+(def result (.execute build "{books {id, title}}"))
 
-(pprint (.getData result))
+(doall
+ (println (.getData result))
+ (println (.getErrors result)))
